cc5a736174e9b8a66affa535eef45b3b4f68624f
Bulan <Bulan>
2016-04-26 16:20:09 +0700

    yang ini

--- maya/maya.php
+++ maya/maya.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+	<pre>
 	<?php
 		$barang = array(
-					array("Laptop"=>"Acer"),
-					array("Mouse"=>"Razer"),
-					array("Keyboard"=>"Genius")
+					array("Laptop","Acer"),
+					array("Mouse","Razer"),
+					array("Keyboard","Genius")
 					);
+		
+		rsort($barang);
 		print_r ($barang);	
+		
 	?> 
+</pre>
 </body>
 </html>
