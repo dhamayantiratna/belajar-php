18bb7e89e72e104832e97e5185a578a72a5fb0a6
mbieb <muthaarif@gmail.com>
2016-04-26 16:13:10 +0700

    up

--- mbieb/mbieb.php
+++ mbieb/mbieb.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<pre>
 	 	<?php
 			$hero = array
 			(
 
-				array("OD",2),
-				array("WR",3),
-				array("Zeus",1)
+				array("OD",2,"mid"),
+				array("WR",3,"mid"),
+				array("Zeus",1,"mid")
 
 			);
-
+			
 			print_r($hero);
+
 			?> 
-	
+</pre>
 </body>
 </html>
