f51349b76e97c252fc77c0d63766b557a9c027d8
Bulan <Bulan>
2016-04-26 15:16:05 +0700

    coba

--- belajar.php
+++ belajar.php
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <body>
 
 	<p>Hello World</p>
+	<p>dududu<p>
 </body>
-=======
-<!DOCTYPE html>
-<html>
-<body>
-
-	
-</body>
->>>>>>> e01ff88bcaa78dfb14abcb102e34c49d4a173e56
 </html>
