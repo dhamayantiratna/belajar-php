216d559142dc8ce457af5c508239b373ad5cb968
Bulan <Bulan>
2016-04-26 19:07:31 +0700

    numpang save

--- maya/maya.php
+++ maya/maya.php
@@ -3,17 +3,28 @@
 <body>
 	<pre>
 	<?php
-		$barang = array(
-					'Acer'=>'1000',
-					'Asus'=>'2000',
-					'Lenovo'=>'3000');
+		$harga = array("Acer"=>"1000","Asus"=>"2000","Lenovo"=>"3000");
+		echo '<table border="1">';
+					echo '<tr>';
+						echo '<th>';
+							echo 'Merk';
+						echo '</th>';
+						echo '<th>';
+							echo 'Harga';
+						echo '</th>';
+					echo '</tr>';
+		foreach($harga as $a => $b){
+				echo '<td>'.$a.'</td>';
+				echo '<td>'.$b.'</td>';
+			echo '<tr>';
+			echo '</tr>';
+		}	
+			echo '</table>';
+			
 		
-		//rsort($barang);
-		print_r ($barang);	
-		//echo 'You have' .$barang['Laptop'];
-		
-		
-	?> 
+
+		?>
+
 </pre>
 </body>
 </html>
