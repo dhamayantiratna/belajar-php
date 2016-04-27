396fa98a80b4e62fbc78c05c87e7dbd09f3668ed
mbieb <muthaarif@gmail.com>
2016-04-27 19:25:15 +0700

    tabel

--- mbieb/mbieb.php
+++ mbieb/mbieb.php
@@ -1,18 +1,64 @@
 <!DOCTYPE html>
 <html>
 <body>
-<pre>
+
 	 	<?php
 			$hero = array(
 			
-				array("nama"=>"OD","lane"=>"mid"),
-				array("nama"=>"WR","lane"=>"bot"),
-				array("nama"=>"Zeus","lane"=>"top")
+				array("nama"=>"OD","lane"=>"mid","tipe"=>"range", "Pro"=>true),
+				array("nama"=>"WR","lane"=>"bot","tipe"=>"range","Pro"=>false),
+				array("nama"=>"Zeus","lane"=>"top","tipe"=>"range","Pro"=>true)
 			);
-			
-			print_r($hero);
 
-			?> 
-</pre>
+			?>
+
+			<table border='1' style='width:100%'>
+				<thead>
+					<th>
+						Nama
+				</th>
+
+					<th>
+						Lane
+					</th>
+
+					<th>
+						Tipe
+					</th>
+
+					<th>
+						Pro
+					</th>
+
+					</thead>
+
+				<?php
+				foreach ($hero as $a):
+
+				?>
+						<tr>
+							<td> <?php
+					echo $a['nama'];  ?>
+						</td>
+							<td> <?php
+					echo $a['lane']; ?>
+							</td>
+						<td> <?php
+					echo $a['tipe']; ?>
+						</td> 
+
+						<td> <?php
+					echo $a['Pro']; ?>
+						</td> 
+
+				</tr>
+				
+				<?php
+				 endforeach;
+				 ?>
+			</table>
+
+			
+
 </body>
 </html>
